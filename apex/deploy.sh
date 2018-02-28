598cc4bf522fadda583e060f929bab4011ba9fbd
Marina Frank <marinasfrank@gmail.com>
2018-02-28 17:55:21 +0000

    Deploy script working version

--- apex/deploy.sh
+++ apex/deploy.sh
@@ -1,25 +1,48 @@
 #!/bin/bash
 #todo param dbdiff disable<a
+set -x
+LOG=deploy.log 
+:>$LOG
+
+_Log() {
+  # write msg into log file and stdout
+  echo "$*" | tee -a $LOG
+}
+
 TARGET_SCHEMA="HOMEFINANCE"
-RUNSQL=$'sqlplus  -s "/ as sysdba" << EOF
+RUNSQL=$'sqlplus -s / as sysdba << EOF
   set heading off trimout on feedback off verify off pages 0\n
+  spool $LOG append\n
   WHENEVER SQLERROR EXIT SQL.SQLCODE\n
   ALTER SESSION SET CURRENT_SCHEMA=$TARGET_SCHEMA;\n
   $SQLQUERY\n
   exit;\n
 EOF'
 
-DBDIFFLAST=`ls sql/dbdiff -t | head -n 1`
+DBDIFFLAST=$(ls -1 sql/dbdiff/* -t | head -n 1)
 
-echo "Deploying dbdiff $DBDIFFLAST..."
+_Log "Deploying dbdiff $DBDIFFLAST..."
 
-SQLQUERY=$@'
-eval RUNRESULT="${RUNSQL}"
+SQLQUERY="@$DBDIFFLAST"
+eval "${RUNSQL}"
 
-echo "Deploying database API ..."
-eval RUNRESULT="${RUNSQL}"
+ "Deploying database API ..."
+SQLLIST=$(ls -1 sql/05_api/*)
+SQLQUERY=$(sed "s/^/@@/" <<< "$SQLLIST")
+eval "${RUNSQL}"
 
-echo "Deploying APEX application ..."
+_Log "Deploying APEX application ..."
+# default workspace and app_id are defined inside each import script
 
+RUNSQL=$'sqlplus  -s / as sysdba << EOF
+  set serveroutput on\n 
+  spool $LOG append\n
+  exec apex_application_install.set_schema(\'$TARGET_SCHEMA\');\n
+  $SQLQUERY\n
+  exit;\n
+EOF'
 
-
+SQLLIST=$(ls -1 app/deploy/*)
+SQLQUERY=$(sed "s/^/@@/" <<< "$SQLLIST")
+eval "${RUNSQL}"
+_Log "Homefinance deploy finished. See log $LOG"
